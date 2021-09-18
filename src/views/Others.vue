52daefbdc78a369fd9dfd1eb102b5ad1fedcbc57
Cian Lu <lushan123888@gmail.com>
2021-09-19 00:00:35 +0800

    Change Component name

--- src/views/Others.vue
+++ src/views/Others.vue
@@ -37,7 +37,7 @@
 
 <script>
 export default {
-  name: "Home",
+  name: "Others",
   components: {},
   data: function () {
     return {
--- src/views/Skills.vue
+++ src/views/Skills.vue
@@ -37,7 +37,7 @@
 
 <script>
 export default {
-  name: "Home",
+  name: "Skills",
   components: {},
   data: function () {
     return {
